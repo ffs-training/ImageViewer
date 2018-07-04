0f755f48727d027a1cf544a3cffeca710533209b
小寺加南子 <kanako.kodera@fujifilm.com>
2018-07-04 13:40:03 +0900

    コメント追加

--- ImageViewerApp/src/app/common/observer.service.ts
+++ ImageViewerApp/src/app/common/observer.service.ts
@@ -13,6 +13,14 @@ export class ObserverService {
     this.topics = {};
   }
 
+  /**
+   * イベント発生時に実行するコールバックを登録します
+   * 使い方
+   * observerService.addEventLister('testEvent!', this, (test, testtest) => console.log(test + testtest))
+   * @param topic イベントの名前
+   * @param caller イベントリスナーの登録者のインスタンス
+   * @param callback コールバック関数
+   */
   public addEventLister(topic: string, caller: any, callback: Function): void {
     if (!(topic in this.topics)) {
       this.topics[topic] = new Subject();
@@ -22,6 +30,13 @@ export class ObserverService {
     });
   }
 
+  /**
+   * イベントを発火させます
+   * 使い方
+   * observerService.fireEvent('testEvent!', 'test!!', 'testtest!!');
+   * @param topic イベントの名前
+   * @param args イベントリスナーに渡す引数
+   */
   public fireEvent(topic: string, ...args: any[]): void {
     if (topic in this.topics) {
       this.topics[topic].next(args);
