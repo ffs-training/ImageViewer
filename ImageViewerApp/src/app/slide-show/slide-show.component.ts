83b70bde21f62413ccca5426de6f3ab7ea8d4354
yoshikibkomori-ffs <yoshiki.b.komori@fujifilm.com>
2018-07-05 17:00:25 +0900

    演習1完了_小森

--- ImageViewerApp/src/app/slide-show/slide-show.component.ts
+++ ImageViewerApp/src/app/slide-show/slide-show.component.ts
@@ -4,6 +4,7 @@ import { ImageModel } from '../model/image-model';
 import { ImageModelService} from '../model/image-model.service';
 
 import { ObserverService} from '../common/observer.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-slide-show',
@@ -12,9 +13,47 @@ import { ObserverService} from '../common/observer.service';
 })
 export class SlideShowComponent implements OnInit {
 
-  constructor() { }
+  items : ImageModel[];
+  imageIndex : number;
+
+  constructor(private imageModelService:ImageModelService,
+  private observerService: ObserverService) {
+    // this.imageModelService.fetch().subscribe((data) => {
+    //   console.log(data); 
+    //   });
+    // this.imageModelService.fetch().subscribe((data)=>{
+    //   this.items = data;
+    //     });
+    this.imageIndex = 0;
+  }
+   
 
   ngOnInit() {
+    this.imageModelService.fetch().subscribe((data)=>{
+      this.items = data;
+        });
 
+        this.observerService.addEventLister('addTag',this,(tag) =>{
+          
+          this.imageModelService.addtag(this.items[this.imageIndex].id,tag)
+        });
+  }
+
+
+  OnClickR(){
+    if(this.imageIndex < this.items.length - 1){
+      this.imageIndex++;
+    }
+    else{
+    }  
+  }
+
+  OnClickL(){
+    if(this.imageIndex > 0){
+      this.imageIndex--;
+    }
+    else{
+       
+    }  
   }
 }
