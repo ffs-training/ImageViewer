c51e87a3a37dc8843ec6977fc1f76fc1abee38fe
直喜 永石 <naoki.nagaishi@fujifilm.com>
2018-07-05 13:15:05 +0900

    課題1終了
    課題１終了

--- ImageViewerApp/src/app/slide-show/slide-show.component.ts
+++ ImageViewerApp/src/app/slide-show/slide-show.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ImageModel } from '../model/image-model';
 import { ImageModelService} from '../model/image-model.service';
@@ -11,10 +11,43 @@ import { ObserverService} from '../common/observer.service';
   styleUrls: ['./slide-show.component.css']
 })
 export class SlideShowComponent implements OnInit {
+  slideimages:ImageModel[] = new Array<ImageModel>();
+  imageIndex:number ;
+  imageNumber:number;
+  rightMoveEnable:boolean;
+  leftMoveEnable:boolean;
 
-  constructor() { }
+  constructor(private imageModelService:ImageModelService) {
+    this.imageIndex = 0;
+    this.imageNumber = length;
+    this.rightMoveEnable =true;
+    this.leftMoveEnable = false;
+   }
 
+  Onrightslide(event){
+    this.imageIndex +=1;
+    if( this.imageIndex >= 0 && this.imageIndex  < this.imageNumber){
+      this.rightMoveEnable = true;
+    }
+    else {
+      this.rightMoveEnable = false;
+    }
+  }
+  Onleftslide(event){
+    this.imageIndex -=1;
+    if(  this.imageIndex  > 0 && this.imageIndex <= this.imageNumber ){
+      this.leftMoveEnable = true;
+    }
+    else {
+      this.leftMoveEnable = false;
+    }
+
+  }
   ngOnInit() {
-
+    this.imageModelService.fetch().subscribe( 
+      images =>{
+        this.slideimages = images;
+      }
+    )
   }
 }
