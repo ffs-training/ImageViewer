8ad5ede6696bd02dce6caed510000eb9d1018202
moritoniseki-ffs <38636894+moritoniseki-ffs@users.noreply.github.com>
2018-07-05 11:23:19 +0900

    変更しますた(7/5)var2

--- ImageViewerApp/src/app/slide-show/slide-show.component.ts
+++ ImageViewerApp/src/app/slide-show/slide-show.component.ts
@@ -14,13 +14,13 @@ export class SlideShowComponent implements OnInit {
 
   items: ImageModel[];
 
-  index: number;
+  target_index: number;
 
   isleft: boolean;
   isright: boolean;
 
   constructor(private imageModelService :ImageModelService) { 
-    this.index = 0;
+    this.target_index = 0;
     this.isleft = true;
     this.isright = false;
   }
@@ -34,21 +34,21 @@ export class SlideShowComponent implements OnInit {
   }
 
   onright(event){
-    if(this.index < this.items.length-1){ //ここまではindex=8まで受け入れ
-      this.index++; //index=9になる
+    if(this.target_index < this.items.length-1){ //ここまではindex=8まで受け入れ
+      this.target_index++; //index=9になる
       this.isleft = false;
     }
-    if(this.index === this.items.length-1){ //index=9ならば
+    if(this.target_index === this.items.length-1){ //index=9ならば
       this.isright=true;
     }
   }
   
   onleft(event){
-    if(this.index > 0){ //ここまではindex=1まで受け入れ
-      this.index--;　//index=0になる
+    if(this.target_index > 0){ //ここまではindex=1まで受け入れ
+      this.target_index--;　//index=0になる
       this.isright = false;
     }
-    if(this.index === 0){　//index=0ならば
+    if(this.target_index === 0){　//index=0ならば
       this.isleft=true;
     }
   }
